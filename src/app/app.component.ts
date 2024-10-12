0164b9dee24bead4dbd69c3b8e516149b816a3ee
Lalucita <lu.perez09@gmail.com>
2024-10-12 10:28:46 -0400

    Primer commit: Subida inicial del proyecto de angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ApiService } from './services/api.service';
+import { error } from 'console';
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HttpClientModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'inventarioProyecto';
+  companies:any[]=[];
+  constructor(private apiService: ApiService){}
+
+  ngOnInit(): void {
+    this.apiService.getCompanies().subscribe (
+      (data)=>{
+        this.companies=data;
+        console.log('Companies',this.companies);
+      },
+      (error)=>{
+        console.error('Error fetching companies:',error);
+      }
+    );
+  }
 }
